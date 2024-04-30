e0e24421f720a5e7b419822c104435f45fa52625
Amir Granot <digiclick@hotmail.com>
2024-05-01 01:41:39 +0300

    subscriptions init

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,10 +8,11 @@ import { Provider } from 'react-redux'
 
 //TODO: Move the fetching of initial data after login (or retry after so if the user is already logged in it will work as well)
 import store from './redux/store.js'
-import {fetchMovies, fetchMembers, fetchUsers} from './redux/reducer.js'
+import {fetchMovies, fetchMembers, fetchUsers, fetchSubscriptions} from './redux/reducer.js'
 store.dispatch(fetchMovies)
 store.dispatch(fetchMembers)
 store.dispatch(fetchUsers)
+store.dispatch(fetchSubscriptions)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
