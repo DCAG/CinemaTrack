d8b4c57448e4187766c5914b8608aa6b5adf631e
Amir Granot <digiclick@hotmail.com>
2024-05-06 00:07:07 +0300

    added fetching data in main.jsx for app refresh purposes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,6 +10,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import store from './redux/store.js'
+import {fetchMovies, fetchMembers, fetchUsers, fetchSubscriptions} from './redux/reducer.js'
+
+// hitting refresh in the browser will trigger fetching all data (again if already logged in)
+// another fetching is performed after login in the [LoginPage](./pages/LoginPage.jsx)
+store.dispatch(fetchMovies)
+store.dispatch(fetchMembers)
+store.dispatch(fetchUsers)
+store.dispatch(fetchSubscriptions)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
